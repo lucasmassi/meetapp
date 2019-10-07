3ea10869dfab12b8d0d2427b3397a7139f5dabef
Lucas Massi <lucasmassi18@gmail.com>
2019-10-07 17:12:42 -0300

    Add File controller and avatar in users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Guest
 routes.post('/users', UserController.store);
@@ -16,5 +20,7 @@ routes.use(authMiddleware);
 
 // Auth
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
